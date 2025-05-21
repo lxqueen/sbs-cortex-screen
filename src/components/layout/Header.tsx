ed9d0d1714406c3b42772eb92805076c0b28889d
Alexis Queen <alexisqueen@protonmail.com>
2025-05-21 16:29:28 +0100

    Added basic header links

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,38 @@
+import { clsx } from "clsx";
+
+interface NavLink {
+	name: string;
+	link: string;
+}
+
+const NAV_LINKS: NavLink[] = [
+	{ name: "Scene", link: "/" },
+	{ name: "Map", link: "/map" },
+	{ name: "Rules", link: "/rules" },
+];
+
 export function Header() {
 	return (
 		<header className="relative p-gutter pb-0">
+			<nav>
+				<ul className="flex flex-wrap gap-2">
+					{NAV_LINKS.map((link) => (
+						<li key={link.link}>
+							<a
+								className={clsx(
+									"inline-flex items-center px-2 h-8 text-center rounded-sm",
+									link.link === "/"
+										? "bg-primary-loud-fill text-primary-loud-fore border-primary-loud-border"
+										: "bg-primary-quiet-fill text-primary-quiet-fore border-primary-quiet-border",
+								)}
+								href={link.link}
+							>
+								{link.name}
+							</a>
+						</li>
+					))}
+				</ul>
+			</nav>
 		</header>
 	);
 }
