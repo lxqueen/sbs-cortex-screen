2ebbd23d0bfddacf0fd71a26b0dcf5cfcc402b61
Alexis Queen <alexisqueen@protonmail.com>
2025-05-22 16:32:05 +0100

    Page can now have custom classnames

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -1,7 +1,14 @@
+import { clsx } from "clsx";
 import type { PropsWithChildren } from "react";
 
-export interface PageProps extends PropsWithChildren {}
+export interface PageProps extends PropsWithChildren {
+	className?: string;
+}
 
-export function Page({ children }: PageProps) {
-	return <div className="relative h-full p-gutter">{children}</div>;
+export function Page({ className, children }: PageProps) {
+	return (
+		<div className={clsx("relative flex-1 p-gutter", className)}>
+			{children}
+		</div>
+	);
 }
